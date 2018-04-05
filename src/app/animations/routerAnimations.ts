1ed8e86e54a28d03c1804c6c61b9aa4e3c68f642
Ivo Udelsmann <ivo.udelsmann@eldorado.org.br>
2018-04-05 11:32:57 -0300

    Fix width of items, use button for back

--- src/app/animations/routerAnimations.ts
+++ src/app/animations/routerAnimations.ts
@@ -11,12 +11,12 @@ export const routerTransition =
             group([
                 query(':enter', [
                     style({ transform: 'translateY(50%)' }),
-                    animate('0.15s', style({ transform: 'translateY(0%)' }))
+                    animate('0.1s', style({ transform: 'translateY(0%)' }))
                 ], { optional: true }),
 
                 query(':enter', [
                     style({ opacity: 0 }),
-                    animate('0.2s', style({ opacity: 1 }))
+                    animate('0.1s', style({ opacity: 1 }))
                 ], { optional: true }),
             ])
         ]),
@@ -29,7 +29,7 @@ export const routerTransition =
             ]),
             group([
                 query(':leave', [
-                    animate('0.15s', style({ opacity: 0, transform: 'translateY(50%)' }))
+                    animate('0.1s', style({ opacity: 0, transform: 'translateY(50%)' }))
                 ], { optional: true }),
             ])
         ])
